0008de9c462ee9eee89d03a5416915ee79899469
ncikiso023 <ncikiso023@student.wethinkcode.co.za>
2026-01-31 00:27:47 +0200

    changes

--- client/movieApi.js
+++ client/movieApi.js
@@ -1,276 +1,242 @@
-import Alpine from 'alpinejs';
 import axios from "axios";
 
-// Function to get API base URL
-const getApiUrl = () => {
-    // In production (Render), use relative URLs
-    // In development, use localhost
-    return import.meta.env.PROD ? '' : 'http://localhost:4554';
-};
+export default function moviesApp() {
+    return {
+        nav:false,
+        createAcc:false,
+        logUser:true,
+        username: null,
+        usernameReg: null,
+        passwordReg: null,
+        firstname: null,
+        lastname: null,
+        password: null,
+        enterOpen: false,
+        playlistOpen: false,
+        displayMovies:false,
+        displayResults:false,
+        favoriteMovies:false,
+        signUp:false,
+        Login:false,
+        movie_name: null,
+        movies: [],
+        favPlaylists: [],
+        original_title: null,
+        movie_id: null,
+        poster_img: null,
+        user: null,
+        // errors
+        errorText: '',
+        successErr: '',
+        user:'',
+        loginSuccessMsg: null,
+        noMovies: null,
+        registerSuccessMsg:null,
+        addedList:null,
+     //working on the login button 
 
-const API_BASE = getApiUrl();
 
-Alpine.data('enterOpen', () => ({
-    nav: false,
-    createAcc: false,
-    logUser: true,
-    username: null,
-    usernameReg: null,
-    passwordReg: null,
-    firstname: null,
-    lastname: null,
-    password: null,
-    enterOpen: false,
-    playlistOpen: false,
-    displayMovies: false,
-    displayResults: false,
-    favoriteMovies: false,
-    signUp: false,
-    Login: false,
-    movie_name: null,
-    movies: [],
-    favPlaylists: [],
-    original_title: null,
-    movie_id: null,
-    poster_img: null,
-    user: null,
-    // errors
-    errorText: '',
-    successErr: '',
-    loginSuccessMsg: null,
-    noMovies: null,
-    registerSuccessMsg: null,
-    addedList: null,
-    token: null,
-
-    // Check if movie is already saved
-    isSaved(movieId) {
-        return this.favPlaylists && this.favPlaylists.some(movie => movie.movie_id === movieId);
-    },
-
-    init() {
-        console.log(localStorage['user']);
+     init() {
+        console.log(localStorage['users']);
         if (localStorage['user'] !== undefined) {
-            this.logUser = false;
+
+           
+            this.logUser= false
             this.user = JSON.parse(localStorage.getItem('user'));
-            console.log("------------------", this.user);
-            
-            if (this.user) {
-                this.displayMovies = false;
-                this.displayResults = false;
-                this.nav = true;
-            }
-        }
-        
-        // Load token if exists
-        const token = localStorage.getItem('access_token');
-        if (token) {
-            this.token = token;
+            console.log("------------------", this.user)
+            console.log(this.user.role)
+            this.signIn.username = this.user.username
+            if (this.user === "users") {
+                this.displayMovies=false
+                this.displayResults=false
+                this.nav= true
+            } 
         }
     },
+        logIn() {
+           
+            const username = this.username
+            const password = this.password
+            axios
 
-    async logIn() {
-        const username = this.username;
-        const password = this.password;
-
-        try {
-            const response = await axios.post(`${API_BASE}/api/login`, { username, password });
+                .post("http://localhost:4554/api/login", { username, password })
+                .then((users) => {
+                               
+                                const { userInfo } = users.data
+                            
+                                var { access_token } = users.data
+                                    console.log(access_token);
+                                if (users.data.success == true) {
+                                    this.loginSuccessMsg = 'Successfully login'
+                                    this.displayMovies = false
+                                    this.displayResults=true
+                                    this.nav = true
+                                    this.logUser = false
+                                    // this.favoriteMovies= false
+                                    this.token = access_token
+                                    this.user = users.data.user.userid
             
-            if (response.data.success === true) {
-                this.loginSuccessMsg = 'Successfully logged in';
-                this.displayMovies = false;
-                this.displayResults = true;
-                this.nav = true;
-                this.logUser = false;
-                
-                this.token = response.data.access_token;
-                this.user = response.data.user.userid;
+                                    console.log(this.user + "", "xdcfghujki");
+                        localStorage.setItem('access_token', this.token)
 
-                console.log(this.user + "", "logged in user");
-                
-                // Store in localStorage
-                localStorage.setItem('access_token', this.token);
-                localStorage.setItem('user', JSON.stringify(response.data.user));
-                
-            } else {
-                this.loginSuccessMsg = 'Invalid username or password';
-            }
-        } catch (error) {
-            console.error('Login error:', error);
-            this.loginSuccessMsg = 'Login failed. Please try again.';
-        }
+                    } else if(!userInfo) {
+                        // this.logUser = false
+                        // this.enterOpen = false
+                        // this.errorText = 'Please sign up'
+                        // this.username = ''
+                        // this.password = ''
+                        return false
+                      
+                    }
+                    
+                    
 
-        setTimeout(() => {
-            this.loginSuccessMsg = '';
-        }, 3000);
-    },
+                })
+                setTimeout(() => {
+                    this.loginSuccessMsg = '';
+                }, 3000);
+        },
+ 
+        register() {
+            const username = this.usernameReg
+            const password = this.passwordReg
+            const firstname = this.firstname
+            const lastname = this.lastname
 
-    async register() {
-        const username = this.usernameReg;
-        const password = this.passwordReg;
-        const firstname = this.firstname;
-        const lastname = this.lastname;
-
-        try {
-            const response = await axios.post(`${API_BASE}/api/register`, {
-                username,
-                password,
-                firstname,
-                lastname
-            });
-
-            this.registerSuccessMsg = 'Successfully registered! Please login.';
-            this.usernameReg = '';
-            this.passwordReg = '';
-            this.firstname = '';
-            this.lastname = '';
-
-            // Switch to login form after 2 seconds
+            axios
+                .post("http://localhost:4554/api/register", {
+                    username,
+                    password,
+                    firstname,
+                    lastname
+                })
+                // .then(App => App.json())
+                .then({
+                    username: this.username,
+                    firstname: this.firstname,
+                    lastname: this.lastname,
+                    password: this.password
+                })
+                .then(App => console.log(App.data))
+             this.registerSuccessMsg = 'Successfully registered'
+            this.usernameReg = ''
+            this.passwordReg = ''
+            this.firstname = ''
+            this.lastname = ''
+            
             setTimeout(() => {
-                this.createAcc = false;
-                this.logUser = true;
-            }, 2000);
+                this.registerSuccessMsg = '';
+            }, 3000);
+        },
+        logOut(){
+            localStorage.clear();
+            this.logUser = true
+            this.displayMovies=false
+            this.favPlaylists = false
+            this.favoriteMovies =false
+            location.reload();
+        },
+        maindisplay(){
+            axios
+                .get(`https://api.themoviedb.org/3/search/movie?api_key=02d4c4c93174c2c6c4871f23e91933c6&query=${this.movies}`)
+                .then((App)=>{
+                    this.movies = App.data.results
+                })
 
-        } catch (error) {
-            console.error('Registration error:', error);
-            this.registerSuccessMsg = 'Registration failed. Username may already exist.';
-        }
+        },
+        playlist() {
 
-        setTimeout(() => {
-            this.registerSuccessMsg = '';
-        }, 3000);
-    },
+            axios
+                .get(`https://api.themoviedb.org/3/search/movie?api_key=02d4c4c93174c2c6c4871f23e91933c6&query=${this.movie_name}`)
 
-    logOut() {
-        localStorage.clear();
-        this.logUser = true;
-        this.displayMovies = false;
-        this.favPlaylists = [];
-        this.favoriteMovies = false;
-        this.nav = false;
-        this.user = null;
-        this.token = null;
-        location.reload();
-    },
-
-    async playlist() {
-        if (!this.movie_name || this.movie_name.trim() === '') {
-            this.noMovies = 'Please enter a search term';
+            .then((App) => {
+              
+                console.log(App.data.results);
+                // this.movies = App.data.results
+                // console.log(this.movies);
+                if (App.data.results != 0) {
+                this.movies = App.data.results
+                this.displayMovies = true
+               
+                    
+                }else{
+                    this.noMovies = 'No results'
+                  
+                 return false
+                }
+            })
             setTimeout(() => {
                 this.noMovies = '';
             }, 3000);
-            return;
-        }
 
-        try {
-            const response = await axios.get(
-                `https://api.themoviedb.org/3/search/movie?api_key=02d4c4c93174c2c6c4871f23e91933c6&query=${this.movie_name}`
-            );
-
-            console.log(response.data.results);
-
-            if (response.data.results && response.data.results.length > 0) {
-                this.movies = response.data.results;
-                this.displayMovies = true;
+        },
+        filter(){
+            axios
+            .get(`https://api.themoviedb.org/3/search/movie?api_key=02d4c4c93174c2c6c4871f23e91933c6&query=${this.animation,this.comedy,this.action}`)
+            then((App)=> {
+                console.log(App.data);
+                this.animation = App.data.result
+            })
+            setTimeout(() => {
                 this.noMovies = '';
-            } else {
-                this.noMovies = 'No results found';
-                this.movies = [];
-            }
-        } catch (error) {
-            console.error('Search error:', error);
-            this.noMovies = 'Error searching movies';
-        }
-
-        setTimeout(() => {
-            this.noMovies = '';
-        }, 3000);
-    },
-
-    async favouriteMovie() {
-        console.log(this.original_title);
-        console.log(this.movie_id);
-        console.log(this.poster_img);
-
-        const movie_id = this.movie_id;
-        const movie_title = this.original_title;
-        const img = this.poster_img;
-        const user_id = this.user;
-
-        try {
-            const response = await axios.post(`${API_BASE}/api/playlist`, { 
-                movie_id, 
-                movie_title, 
-                img, 
-                user_id 
-            });
-
-            console.log(response.data);
-            
-            // Add movie to local favPlaylists immediately
-            if (!this.isSaved(movie_id)) {
-                this.favPlaylists.push({
-                    movie_id: movie_id,
-                    movie_title: movie_title,
-                    img: img,
-                    user_id: user_id,
-                    id: response.data.id // Get the database ID
-                });
-            }
-
-            this.addedList = '✓ Successfully added to your library!';
-
-        } catch (error) {
-            console.error('Error adding movie:', error);
-            this.addedList = '✗ Failed to add movie';
-        }
-
-        setTimeout(() => {
-            this.addedList = '';
-        }, 3000);
-    },
-
-    async Addplaylist() {
-        if (!this.user) {
-            console.log('No user logged in');
-            return;
-        }
-
-        try {
-            const response = await axios.get(`${API_BASE}/api/playlist/${this.user}`);
-            
-            console.log(response.data);
-            this.favPlaylists = response.data.result || [];
-            this.movies = false;
-
-        } catch (error) {
-            console.error('Error loading playlist:', error);
-            this.favPlaylists = [];
-        }
-    },
-
-    async deleteMovie(element) {
-        try {
-            await axios.delete(`${API_BASE}/api/playlist/${element.id}`);
-            
-            // Remove from local array immediately
-            this.favPlaylists = this.favPlaylists.filter(movie => movie.id !== element.id);
-            
-            this.addedList = '✓ Movie removed from library';
-            
-            setTimeout(() => {
-                this.addedList = '';
             }, 3000);
-
-        } catch (error) {
-            console.error('Error deleting movie:', error);
-            this.addedList = '✗ Failed to remove movie';
             
-            setTimeout(() => {
-                this.addedList = '';
-            }, 3000);
-        }
+        },
+        favouriteMovie() {
+
+            console.log(this.original_title);
+            console.log(this.movie_id);
+            console.log(this.poster_img);
+            const movie_id = this.movie_id
+            const movie_title = this.original_title
+            const img = this.poster_img
+            const user_id = this.user
+
+            axios
+
+                .post("http://localhost:4554/api/playlist", { movie_id, movie_title, img, user_id })
+                .then((App) => {
+                    console.log(App.data);
+                this.addedList='successfully added'
+                if(this.movies.indexOf(App.data) === -1){
+                    this.movies.push(App.data)
+                }
+                    console.log('am about to work' + this.movies);
+                })
+                setTimeout(() => {
+                    this.addedList = '';
+                }, 3000);
+
+        },
+        Addplaylist() {
+            axios
+                .get(`http://localhost:4554/api/playlist/${this.user}`)
+                .then((App) => {
+                  
+                    console.log(App.data);
+                    this.favPlaylists = App.data.result
+                    this.movies= false
+
+                })
+
+
+        },
+       
+        //to do today 
+        deleteMovie(element) {
+
+            try {
+                // console.log(this.id);
+                axios.delete(`http://localhost:4554/api/playlist/${element.id}`)
+
+
+                .then(() => this.Addplaylist())
+                    // .then(console.log(this.addClothing))
+            } catch (error) {
+
+            }
+        },
+
     }
-}));
 
-Alpine.start();
+}
