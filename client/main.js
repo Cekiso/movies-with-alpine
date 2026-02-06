1d30e1a8530b9f2d4a443437fba52cd097e13afa
ncikiso023 <ncikiso023@student.wethinkcode.co.za>
2026-02-06 14:07:37 +0200

    .

--- client/main.js
+++ client/main.js
@@ -1,6 +1,10 @@
 import './style.css'
 import './movies.css'
 import Alpine from 'alpinejs'
+import moviesApp from '../moviesApp.js';
+
+// Make it available globally for Alpine
+window.moviesApp = moviesApp;
 
 import moviesApp from './movieApi'
 Alpine.data('enterOpen', moviesApp)
